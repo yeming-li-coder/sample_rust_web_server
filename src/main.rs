036b5cf970d3d2d9a307967ef440d99a34c3736a
ymli <chaoch2000@gmail.com>
2023-10-08 12:48:38 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-mod processes;
-mod state;
-mod to_do;
-use serde_json::json;
-use std::env;
-use to_do::enums::TaskStatus;
-use to_do::to_do_factory;
-use to_do::traits::delete::Delete;
-use to_do::traits::edit::Edit;
-use to_do::traits::get::Get;
-use to_do::ItemTypes;
+use std::collections::HashMap;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let status = &args[1];
-    let title = &args[2];
-    let mut state = state::read_file("./state.json");
-    println!("Before operation: {:?}", state);
+use axum::extract::{Path, Query};
+use axum::{routing::get, Router};
+async fn greet() -> String {
+    "Hello World!".to_owned()
+}
 
-    let status = {
-        match state.get(title) {
-            Some(result) => {
-                result.to_string().replace('\"', "")
-            },
-            None => {
-                "pending".to_string()
-            }
-        }
-    };
+async fn handler(Path(path): Path<HashMap<String, String>>) -> String {
+    match path.get("name") {
+        Some(path) => format!("Hello {}", path),
+        None => format!("Hello World"),
+    }
+}
 
-    let item = to_do_factory(title, TaskStatus::from_string(status.to_uppercase()));
-    state.insert(title.clone(), json!(status));
-    println!("After operation: {:?}", state);
-    state::write_to_file("./state.json", &mut state)
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:name", get(handler))
+        .route("/say/hello", get(|| async { format!("Hello Again!") }));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
